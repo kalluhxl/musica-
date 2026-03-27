d406bd555f6f027ef5c12dfa45a5dc6d8acb7059
Ana Carolina <kalluhxl@gmail.com>
2026-03-26 22:24:53 -0300

    musicas com cores

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Flor De Tangerina");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Hoje eu sonhei que ela voltava");
+Console.WriteLine("E vinha muito mais que linda");
+Console.WriteLine("À meia luz me acordava");
+Console.WriteLine("Cheirando a flor de tangerina");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Eu lhe amava e mergulhava");
+Console.WriteLine("No seu olhar de onça menina");
+Console.WriteLine("E docemente me afogava");
+Console.WriteLine("Em suas águas cristalinas");
+Console.ResetColor();
